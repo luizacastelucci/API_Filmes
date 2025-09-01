934a9e2cfa0215fe47bbd88de31e586031c5bf62
luizacastelucci <luizafregonesi@gmail.com>
2025-09-01 16:42:13 -0300

    Atualizações

--- model/filmes.py
+++ model/filmes.py
@@ -1,8 +1,8 @@
 from model.database import Database
 from fastapi import HTTPException
-
+ 
 db = Database()
-
+ 
 class API_Filmes:
     def __init__(self, movie_id: int = None, searchTerm: str = None, poster_url: str = None, count: int = None, item: dict = None):
         self.movie_id = movie_id
@@ -10,15 +10,15 @@ class API_Filmes:
         self.poster_url = poster_url
         self.count = count
         self.item = item
-
-    def consultarSerie(self):
+ 
+    def consultar(self):
         db.conectar()
         try:
             if self.movie_id is None:
                 sql = "SELECT * FROM api_filmes"
                 params = ()
             else:
-                sql = "SELECT * FROM api_filmes WHERE id = %s"
+                sql = "SELECT * FROM api_filmes WHERE movie_id = %s"
                 params = (self.movie_id,)
             resultado = db.executar_consulta(sql, params, fetch=True)
             if not resultado:
@@ -28,54 +28,54 @@ class API_Filmes:
             raise HTTPException(status_code=500, detail=f"Erro ao consultar o banco de dados: {str(e)}")
         finally:
             db.desconectar()
-
-    def inserirSerie(self):
+ 
+    def inserir(self):
         """Adiciona um item a uma tabela específica no banco de dados."""
         db.conectar()
-
+ 
         try:
             if not self.item:
                 raise HTTPException(status_code=400, detail="Nenhum dado fornecido para adicionar")
             # Erro 400: Bad Request
-
-            colunas = ', '.join(self.item.keys()) 
+ 
+            colunas = ', '.join(self.item.keys())
             # para cada chave do dicionário, cria uma string separada por vírgula
-            valores = ', '.join(['%s'] * len(self.item)) 
+            valores = ', '.join(['%s'] * len(self.item))
             # para cada valor do dicionário, cria uma string no formato "%s", separando por vírgula. .join = juntar strings
-            sql = f"INSERT INTO {self.table_name} ({colunas}) VALUES ({valores})"
+            sql = f"INSERT INTO api_filmes ({colunas}) VALUES ({valores})"
             params = tuple(self.item.values())
-
+ 
             db.executar_consulta(sql, params)
             db.desconectar()
-        
+       
         except Exception as e:
             db.desconectar()
             raise HTTPException(status_code=500, detail=f"Erro ao adicionar o item: {str(e)}")
             # Erro 500: Internal Server Error
-        
-    def removerSerie(self):
+       
+    def remover(self):
         '''Remove um item de alguma lista do banco de dados'''
         db.conectar()
-
+ 
         try:
             if self.movie_id is None:
                 raise HTTPException(status_code=403, detail="Mudança não permitida (Não foi atribuído um ID)")
                 # Erro 403: Forbidden
-
+ 
             sql = f"DELETE FROM {self.table_name} WHERE {self.movie_id} = %s"
             params = (self.movie_id,)
-
+ 
             db.executar_consulta(sql, params)
             db.desconectar()
-
+ 
         except Exception as e:
             db.desconectar()
             raise HTTPException(status_code=500, detail=f"Erro ao remover o item: {str(e)}")
-        
+       
     def atualizarSerie(self):
         '''Atualiza um item de alguma lista do banco de dados'''
         db.conectar()
-
+ 
         try:
             if self.movie_id is None:
                 raise HTTPException(status_code=403, detail="Mudança não permitida (Não foi atribuído um ID)")
@@ -83,12 +83,12 @@ class API_Filmes:
             if not self.item:
                 raise HTTPException(status_code=400, detail="Nenhum dado fornecido para atualização")
                 # Erro 400: Bad Request
-            
-            set_clause = ", ".join([f"{key} = %s" for key in self.item.keys()]) 
+           
+            set_clause = ", ".join([f"{key} = %s" for key in self.item.keys()])
             # para cada chave do dicionário, cria uma string no formato "chave = %s", separando por vírgula
             sql = f"UPDATE {self.table_name} SET {set_clause} WHERE {self.movie_id} = %s"
             params = tuple(self.item.values()) + (self.movie_id,)
-
+ 
             db.executar_consulta(sql, params)
             db.desconectar()
         except Exception as e:
